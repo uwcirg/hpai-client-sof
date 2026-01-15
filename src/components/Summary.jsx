3bf6bcad0864b6b83af5f6207104364bd89d1956
Amy Chen <achen2401@gmail.com>
2026-01-15 12:03:50 -0800

    add iframe

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,3 +1,24 @@
+import { useEffect } from "react";
+import { getEnv } from "@util";
 export default function Summary() {
-    return <div>Hello</div>;
+  const handleMessage = (event) => {
+    if (!event) return;
+    console.log("Received a message from " + event.origin + ".");
+  };
+  useEffect(() => {
+    window.addEventListener("message", handleMessage);
+    return window.removeEventListener("message", handleMessage);
+  }, []);
+  return (
+    <section className="summary-wrapper">
+      <iframe
+        src={getEnv("REACT_APP_IFRAME_URL")}
+        title="SOF app iframe"
+        width="100%"
+        height="100%"
+        frameBorder={0}
+        sandbox="allow-scripts allow-forms allow-modals allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-top-navigation"
+      ></iframe>
+    </section>
+  );
 }
