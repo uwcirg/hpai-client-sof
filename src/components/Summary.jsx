82ca01bd94fd3653cae76231ab7a49ca2254d93a
Amy Chen <achen2401@gmail.com>
2026-01-26 12:25:06 -0800

    fix test

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -37,7 +37,7 @@ const Summary = () => {
                 }
               }
             ]
-          })
+          }),
           headers: {
             "Content-Type": "application/json",
           },
