6b33492ed8d291e1d9d16084d43d769468abf798
Amy Chen <achen2401@gmail.com>
2026-02-05 09:45:49 -0800

    put old files back

---
+++ src/launch.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import LaunchContainer from "./containers/LaunchContainer";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>
+  <LaunchContainer />
+  // </React.StrictMode>
+);
